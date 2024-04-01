23bcb21766301d1a94567d96e64ba00c251998bd
TseWangPok_Jerry <126223772+JerryTseee@users.noreply.github.com>
2024-04-01 14:01:29 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+The code you provided implements the SLX (Select Larger or Equal) algorithm, which performs a binary search to find the smallest element in a sorted list A that is greater than or equal to x.
+"""
+def SLX(A, i, j, x):
+    if i == j:
+        if A[i] >= x:
+            return A[i]
+        else:
+            return -1
+    else:
+        mid = (i+j) // 2
+        if A[mid] < x:
+            return SLX(A, mid+1, j, x)
+        elif A [mid] >= x:
+            return SLX(A, i, mid, x)
+
+A = [1,2,4,5,6]
+x = 3
+print(SLX(A,0,5,x))
